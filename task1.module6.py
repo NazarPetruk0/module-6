1634dbaa5895cf4f7275474f2e0ff9daf6d446ad
NazarPetruk0 <154916004+NazarPetruk0@users.noreply.github.com>
2024-03-07 01:24:28 +0200

    Update task1.module6.py

--- task1.module6.py
+++ task1.module6.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+from datetime import datetime as dtdt
 
 class Field:
     def __init__(self, value):
@@ -12,65 +13,64 @@ class Name(Field):
 
 class Phone(Field):
     def __init__(self, value):
-        if not value.isdigit() or len(value) != 10:
-            print("Invalid phone number format")
-        super().__init__(value)
+        if not (value.isdigit() or len(value) == 10):
+            raise ValueError
+        self.value=value
+
+class Birthday(Field):
+    def __init__(self, value):
+        try:
+            self.value = dtdt.strftime(value, '%d.%m.%Y')
+        except ValueError:
+            raise ValueError
+        
+    def __str__(self):
+        return self.value.strftime('%d.%m.%Y')
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
+        self.birthday = None
 
-    def add_phone(self, phone_number):
-        phone = Phone(phone_number)
-        self.phones.append(phone)
+    def add_phone(self, phone):
+        ph = Phone(phone)
+        self.phones.append(ph)
+        return ph
 
-    def delete_phone(self, phone_number):
-        self.phones = [phone for phone in self.phones if phone.value != phone_number]
+    def add_birthday(self, birthday:str):
+        self.birthday = Birthday(birthday)
 
-    def edit_phone(self, old_phone, new_phone):
-        self.delete_phone(old_phone)
-        self.add_phone(new_phone)
+    def get_upcoming_birthdays(self):
+        return []
 
-    def find_phone(self, phone_number):
-        for phone in self.phones:
-            if phone.value == phone_number:
-                return phone.value
+    def find_phone(self, phone:str):
+        for ph in self.phones:
+            if ph.value == phone:
+                return ph
         return None
 
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+    def remove_phone(self,phone:str):
+        self.phones.remove(self.find_phone(phone))
+
+def edit_phone(self, old_phone:str, new_phone:str):
+    if self.find_phone(old_phone):
+        self.remove_phone(old_phone)
+        self.add_phone(new_phone)
+    else:
+        raise ValueError
+    
+def __str__(self):
+    return f"Contact name: {str(self.name)}, phones: {'; '.join(str(p) for p in self.phones)}, birthday"
 
 class AddressBook(UserDict):
-    def add_record(self, record):
+    def add_record(self, record:Record):
         self.data[record.name.value] = record
+        return record
 
-    def delete(self, name):
-        del self.data[name]
+    def delete(self, name:str):
+        if self.find(name):
+            del self.data[name]
 
-    def find(self, name):
+    def find(self, name:str):
         return self.data.get(name)
-
-# Приклад використання:
-book = AddressBook()
-
-john_record = Record("John")
-john_record.add_phone("1234567890")
-john_record.add_phone("5555555555")
-book.add_record(john_record)
-
-jane_record = Record("Jane")
-jane_record.add_phone("9876543210")
-book.add_record(jane_record)
-
-for name, record in book.data.items():
-    print(record)
-
-john = book.find("John")
-john.edit_phone("1234567890", "1112223333")
-print(john)
-
-found_phone = john.find_phone("5555555555")
-print(f"{john.name.value}: {found_phone}")
-
-book.delete("Jane")
