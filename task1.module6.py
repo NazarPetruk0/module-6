da8ed5e5c802efdc058a0619027c9b7eab2242a8
NazarPetruk0 <154916004+NazarPetruk0@users.noreply.github.com>
2024-03-06 19:35:22 +0200

    Add files via upload

---
+++ task1.module6.py
@@ -0,0 +1,76 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            print("Invalid phone number format")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone_number):
+        phone = Phone(phone_number)
+        self.phones.append(phone)
+
+    def delete_phone(self, phone_number):
+        self.phones = [phone for phone in self.phones if phone.value != phone_number]
+
+    def edit_phone(self, old_phone, new_phone):
+        self.delete_phone(old_phone)
+        self.add_phone(new_phone)
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone.value
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def delete(self, name):
+        del self.data[name]
+
+    def find(self, name):
+        return self.data.get(name)
+
+# Приклад використання:
+book = AddressBook()
+
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+book.add_record(john_record)
+
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+for name, record in book.data.items():
+    print(record)
+
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+print(john)
+
+found_phone = john.find_phone("5555555555")
+print(f"{john.name.value}: {found_phone}")
+
+book.delete("Jane")
